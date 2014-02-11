ba63bed82aa9bf7d39357d6f47902c9b391cec5b
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-02-10 22:32:27 -0500

    Create recSys.m

---
+++ recSys.m
@@ -0,0 +1,43 @@
+% Load ratings matrix (users x movies)
+[ratingsMatrix, movieID] = makeRatingsMatrix('recsys-data-ratings.csv');
+
+% Binarize ratings matrix (user i rated movie j if ratingsMatrix(i, j) = 1)
+ratingsMatrix = ~isnan(ratingsMatrix);
+
+% Find similar movies to these
+target = [3049 107 1891];
+
+% Movie declarations
+nSimilar = 5;
+nTarget = length(target);
+nTotalMovie = length(movieID);
+
+% Initialize empty results struct
+top.both.prop = {};
+top.both.movies = {};
+top.others.prop = {};
+top.others.movies = {};
+
+% For every target movie...
+for m = 1:nTarget
+    
+    % ...find which users rated it
+    movieInd = find(target(m) == movieID);
+    rated = ratingsMatrix(:, movieInd);
+    ratedMatrix = repmat(rated, 1, nTotalMovie);
+    
+    % ...and how often they rated every other movie
+    propBoth = sum(ratingsMatrix & ratedMatrix) / sum(rated);
+    propBoth(movieInd) = [];
+    [sortPropBoth sortInd] = sort(propBoth, 'descend');
+    top.both.prop{m} = sortPropBoth(1:nSimilar);
+    top.both.movies{m} = movieID(sortInd(1:nSimilar))';
+    
+    % ...normalize by proportion of users who did not watch target movie
+    propOthers = sum(ratingsMatrix & ~ratedMatrix) / sum(~rated);
+    propOthers(movieInd) = [];
+    [sortPropOthers sortInd] = sort(propBoth ./ propOthers, 'descend');
+    top.others.prop{m} = sortPropOthers(1:nSimilar);
+    top.others.movies{m} = movieID(sortInd(1:nSimilar))';
+    
+end
